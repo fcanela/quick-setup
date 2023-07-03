2763c0bb335de633500f923d365aad1f5d57e355
Francisco Canela <fcanela.dev@gmail.com>
2023-07-03 17:42:07 +0200

    fix: corrects the quick setup chmod

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
 
 #
@@ -19,7 +19,7 @@ fi
 # better to avoid giving too many details about my internal systems
 #
 
-temp_dir=$(mktemp -d) || { echo "Error: Failed to create temporary directory." >&2; exit 1; }
+TEMP_DIR=$(mktemp -d) || { echo "Error: Failed to create temporary directory." >&2; exit 1; }
 read -p "Enter your lpass email: " lpass_email
 
 DEPLOY_KEYS_NOTE_ID=5247608367483238149
@@ -27,25 +27,25 @@ DEPLOY_KEYS_ATTACHMENT_ID=att-5247608367483238149-1618
 
 lpass login "$lpass_email" || { echo "Error: Failed to log in using 'lpass'." >&2; exit 1; }
 
-mkdir "$temp_dir/keys"
-lpass show --note $DEPLOY_KEYS_NOTE_ID --attach=$DEPLOY_KEYS_ATTACHMENT_ID -q > "$temp_dir/keys/all.zip" || { echo "Error: Failed to download deploy keys." >&2; exit 1; }
-unzip "$temp_dir/keys/all.zip" -d "$temp_dir/keys"
-chmod 600 "$temp_dir/*" || { echo "Error: Failed to set file permissions for the downloaded key files." >&2; exit 1; }
+mkdir "$TEMP_DIR/keys"
+lpass show --note $DEPLOY_KEYS_NOTE_ID --attach=$DEPLOY_KEYS_ATTACHMENT_ID -q > "$TEMP_DIR/keys/all.zip" || { echo "Error: Failed to download deploy keys." >&2; exit 1; }
+unzip "$TEMP_DIR/keys/all.zip" -d "$TEMP_DIR/keys"
+chmod 600 $TEMP_DIR/keys/* || { echo "Error: Failed to set file permissions for the downloaded key files." >&2; exit 1; }
 
 #
 # SYSTEM SETUP
 #
-GIT_SSH_COMMAND="ssh -i $temp_dir/system-config -o IdentitiesOnly=yes" git clone git@github.com:fcanela/system-config.git "$temp_dir/system-config" || { echo "Error: Failed to clone the repository." >&2; exit 1; }
-cd "$temp_dir/system-config"
+GIT_SSH_COMMAND="ssh -i $TEMP_DIR/keys/system-config -o IdentitiesOnly=yes" git clone git@github.com:fcanela/system-config.git "$TEMP_DIR/system-config" || { echo "Error: Failed to clone the repository." >&2; exit 1; }
+cd "$TEMP_DIR/system-config"
 ./setup.sh || { echo "Error: Failed to run setup.sh." >&2; exit 1; }
 
 #
 # USER SETUP
 #
-GIT_SSH_COMMAND="ssh -i $temp_dir/dotfiles -o IdentitiesOnly=yes" sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:fcanela/dotfiles.git
+GIT_SSH_COMMAND="ssh -i $TEMP_DIR/keys/dotfiles -o IdentitiesOnly=yes" sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:fcanela/dotfiles.git
 
 #
 # Clean up
 #
 lpass logout -f
-rm -rf "$temp_dir"
+rm -rf "$TEMP_DIR"
